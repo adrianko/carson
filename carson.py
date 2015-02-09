d83c896d9882d1feee5ba9aeeaf8b1499f660007
Adrian Ko <adrianchko@gmail.com>
2015-02-09 10:42:55 +0000

    add todo

--- carson.py
+++ carson.py
@@ -12,6 +12,7 @@ TODO List registered repos
 TODO Add repo unregister
 TODO Add repo modify
 TODO Check if repo already registered when registering
+TODO Suppress output if up to date and display custom message
 """
 def docs():
     print "pull <repo>, <repo> ...    Pull commits from origin/master on all repos"
